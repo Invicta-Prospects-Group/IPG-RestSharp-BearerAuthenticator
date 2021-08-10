deeb894cb7208cb94368529eed433d550c981608
Paul Sorauer <paul@invictaprospectsgroup.com>
2021-08-10 13:28:26 +1000

    Added dynamic evaluation of Bearer token
    - Added support for evaluating the bearer token only when needed by the rest client. This is useful in certain situations where dependency injection is being used and the access token is not yet known.

--- IPG.RestSharp.BearerAuthenticator/BearerAuthenticator.cs
+++ IPG.RestSharp.BearerAuthenticator/BearerAuthenticator.cs
@@ -1,11 +1,13 @@
 using RestSharp;
 using RestSharp.Authenticators;
+using System;
 
 namespace IPG.RestSharp.BearerAuthenticator
 {
     public class BearerAuthenticator : IAuthenticator
     {
         private readonly string accessToken;
+        private readonly Func<string> accessTokenEvaluator;
 
         /// <summary>
         /// Creates a new instance of the BearerAuthentication class.
@@ -16,8 +18,22 @@ namespace IPG.RestSharp.BearerAuthenticator
             this.accessToken = accessToken;
         }
 
+        /// <summary>
+        /// Creates a new instance of the BearerAuthentication class (this consructor useful in certain DI scenarios).
+        /// </summary>
+        /// <param name="">A function to get the access token when it's is only known after the the class has been instaniated.</param>
+        public BearerAuthenticator(Func<string> accessTokenEvaluator)
+        {
+            this.accessTokenEvaluator = accessTokenEvaluator;
+        }
+
         public void Authenticate(IRestClient client, IRestRequest request)
         {
+            var realAccessToken = accessToken ?? accessTokenEvaluator.Invoke();
+
+            if (realAccessToken == null)
+                throw new ArgumentNullException(nameof(accessToken));
+
             request.AddOrUpdateParameter("Authorization", "Bearer " + accessToken, ParameterType.HttpHeader);
         }
     }
